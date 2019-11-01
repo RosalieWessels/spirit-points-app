b1dee3b9a87c5be9012832bf1e008bd5beea55ad
Momei Fang <momeifang@Momeis-MacBook-Air.local>
2019-11-01 11:54:23 -0700

    Added variables for all the points of the classes

---
+++ index.js
@@ -0,0 +1,8 @@
+var freshman_SP = 100;
+var sophomore_SP = 10;
+var junior_SP = 10;
+var senior_SP  =10;
+var m_8_SP  =10;
+var m_7_SP = 10;
+
+document.getElementById("freshman-points").value = int(freshman_SP);
