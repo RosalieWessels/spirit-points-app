d63ba27910452b8e1f214e7aa46eb74f0712a784
RosalieWessels <rosalie.wessels@yahoo.com>
2019-11-03 12:20:11 -0800

    Linked the Javascript file to the index.html file, also made the variable work for Freshman class

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var freshman_SP = 100;
+var freshman_SP = 200;
 var sophomore_SP = 10;
 var junior_SP = 10;
 var senior_SP  =10;
 var m_8_SP  =10;
 var m_7_SP = 10;
 
-document.getElementById("freshman-points").value = int(freshman_SP);
+document.getElementById("freshman-points").textContent = freshman_SP;
