805016b0dcdfe7aa41f9b77deda03c5fa61eb412
Momei Fang <momeifang@Momeis-MacBook-Air.local>
2019-11-09 11:24:40 -0800

    Connected spirit points to js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var freshman_SP = 200;
+var freshman_SP = 20;
 var sophomore_SP = 10;
 var junior_SP = 10;
 var senior_SP  =10;
@@ -6,3 +6,9 @@ var m_8_SP  =10;
 var m_7_SP = 10;
 
 document.getElementById("freshman-points").textContent = freshman_SP;
+document.getElementById("sophmore-points").textContent = sophomore_SP
+document.getElementById("junior-points").textContent = junior_SP
+document.getElementById("senior-points").textContent = senior_SP
+
+document.getElementById("m_7-points").textContent = m_7_SP
+document.getElementById("m_8-points").textContent = m_8_SP
