4d1ee6a4142e93a2bf0ca08ca3a448167fb69b16
Momei Fang <momeifang@Momeis-MacBook-Air.local>
2019-11-25 07:49:48 -0800

    Made the toggle button work

--- index.js
+++ index.js
@@ -1,22 +1,85 @@
 //Variables to store the points in for all the grades
-var freshman_SP = 20;
-var sophomore_SP = 30;
-var junior_SP = 10;
-var senior_SP  =10;
+var freshman_SP = [10, "Freshman"];
+var sophomore_SP = [30, "Sophmore"];
+var junior_SP = [20, "Junior"];
+var senior_SP  = [15, "Senior"];
 var m_8_SP  =10;
 var m_7_SP = 10;
 
-var greatest = Math.max(freshman_SP, sophomore_SP, junior_SP, senior_SP);
+var greatest = Math.max(freshman_SP[0], sophomore_SP[0], junior_SP[0], senior_SP[0]);
 var crowned;
-
 var username = "something"
 var pw = "bla"
+var all_grades = [freshman_SP, sophomore_SP, junior_SP, senior_SP];
 
+//toggle button
+function sort(toggle){
+  if (toggle=="points"){
+    all_grades.sort(sortFunction);
+  }
+  else{
+    all_grades = [freshman_SP, sophomore_SP, junior_SP, senior_SP];
+  }
+  elementtext()
+  crowning(all_grades, greatest)
+}
+
+function sortFunction(a, b){
+  if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] > b[0]) ? -1 : 1;
+    }
+}
+
+document.getElementById("grades").onclick = function() {sort("grades")};
+document.getElementById("points").onclick = function() {sort("points")};
+
+//finds the best grade
+function crowning(all_grades, greatest){
+  $("#fi-crown").removeClass("fas fa-crown");
+  $("#se-crown").removeClass("fas fa-crown");
+  $("#th-crown").removeClass("fas fa-crown");
+  $("#fo-crown").removeClass("fas fa-crown");
+  if(all_grades[0][0]==greatest){
+    crowned="first"
+  }
+  else if (all_grades[1][0]==greatest) {
+    crowned="second"
+  }
+  else if (all_grades[2][0]==greatest) {
+    crowned= "third"
+  }
+//gives the grade with the most spirit points a crown
+  if(crowned=="first"){
+    $("#fi-crown").addClass("fas fa-crown");
+  }
+  else if (crowned=="second") {
+    $("#se-crown").addClass("fas fa-crown");
+  }
+  else if(crowned=="third") {
+    $("#th-crown").addClass("fas fa-crown");
+  }
+  else{
+    $("#fo-crown").addClass("fas fa-crown");
+  }
+}
+
+crowning(all_grades, greatest);
 //Sets the text content of the labels to the variable
-document.getElementById("freshman-points").textContent = freshman_SP;
-document.getElementById("sophmore-points").textContent = sophomore_SP
-document.getElementById("junior-points").textContent = junior_SP
-document.getElementById("senior-points").textContent = senior_SP
+function elementtext(){
+  document.getElementById("first-points").textContent = all_grades[0][0];
+  document.getElementById("second-points").textContent = all_grades[1][0];
+  document.getElementById("third-points").textContent = all_grades[2][0];
+  document.getElementById("fourth-points").textContent = all_grades[3][0];
+
+  document.getElementById("first").textContent = all_grades[0][1];
+  document.getElementById("second").textContent = all_grades[1][1];
+  document.getElementById("third").textContent = all_grades[2][1];
+  document.getElementById("fourth").textContent = all_grades[3][1];
+}
+elementtext()
 
 document.getElementById("m_7-points").textContent = m_7_SP
 document.getElementById("m_8-points").textContent = m_8_SP
@@ -29,29 +92,6 @@ window.onclick = function(event) {
     }
 }
 
-
-if(freshman_SP==greatest){
-  crowned="freshman"
-}
-else if (sophomore_SP==greatest) {
-  crowned="sophmore"
-}
-else if (junior_SP==greatest) {
-  crowned="junior"
-}
-
-if("freshman"==crowned){
-  $("#fr-crown").addClass("fas fa-crown");
-}
-else if ("sophmore"==crowned) {
-  $("#so-crown").addClass("fas fa-crown");
-}
-else if("junior"==crowned) {
-  $("#ju-crown").addClass("fas fa-crown");
-}
-else{
-  $("#se-crown").addClass("fas fa-crown");
-}
 
 function validateForm(){
   var x = document.forms["myForm"]["uname"].value;
