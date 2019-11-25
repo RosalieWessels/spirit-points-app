fb4202ae23316d16ab7f504adf373ad2b45e14ad
Momei Fang <momeifang@Momeis-MacBook-Air.local>
2019-11-24 16:40:48 -0800

    Crown goes to greatest number of points

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 //Variables to store the points in for all the grades
 var freshman_SP = 20;
-var sophomore_SP = 10;
+var sophomore_SP = 30;
 var junior_SP = 10;
 var senior_SP  =10;
 var m_8_SP  =10;
 var m_7_SP = 10;
+var greatest = Math.max(freshman_SP, sophomore_SP, junior_SP, senior_SP);
+var crowned;
+
+if(freshman_SP==greatest){
+  crowned="freshman"
+}
+else if (sophomore_SP==greatest) {
+  crowned="sophmore"
+}
+else if (junior_SP==greatest) {
+  crowned="junior"
+}
 
 //Sets the text content of the labels to the variable
 document.getElementById("freshman-points").textContent = freshman_SP;
@@ -21,4 +33,17 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+if("freshman"==crowned){
+  $("#fr-crown").addClass("fas fa-crown");
+}
+else if ("sophmore"==crowned) {
+  $("#so-crown").addClass("fas fa-crown");
+}
+else if("junior"==crowned) {
+  $("#ju-crown").addClass("fas fa-crown");
+}
+else{
+  $("#se-crown").addClass("fas fa-crown");
 }
